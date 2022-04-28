a58b7c4299732efa6cecc5ef86fd1d881823cb2f
Oliver Chang <oliverchang@gmail.com>
2022-04-28 15:30:47 -0400

    added total display

--- bill_tracker.py
+++ bill_tracker.py
@@ -1,3 +1,4 @@
+from re import T
 from tkinter import *
 from tkinter import ttk, messagebox
 from tkinter import filedialog as fd
@@ -6,6 +7,12 @@ import pandas as pd
 import os
 import numpy
 import datetime
+
+'''
+    Future features:
+        - checkboxes that will pertain to weekly, bi-weekly, annual costs (already have monthly)
+            - checking one of these boxes will trigger a field to pop up with the respective field from above
+'''
 
 root = Tk()
 tree = ttk.Treeview(root, show='headings')
@@ -16,13 +23,17 @@ root.title("Handy Bill Handler")
 root.geometry('500x500')
 
 db = {}
+total_sum = []
+global t
+t = 0.0
+
 
 def open_file():
     count = len(db)
     global bill_desc
     global bill_add_cost
     bill_desc = bill.get()
-    bill_add_cost = add_cost.get()
+    bill_add_cost = float(add_cost.get())
     db[count] = (bill_desc, bill_add_cost)
     file = fd.askopenfilename(title='Select A File')
     clear_all()
@@ -32,13 +43,14 @@ def open_file():
     nan_val = float("NaN")                              # define NaN value to search for
     rf.replace('', nan_val, inplace=True)
     rf.dropna(how='all',axis='columns', inplace=True)   # dropna(how= 'any or 'all' where it has at least one NA, axis= 'columns',0 or 'index',1 Drop columns or rows, inplace=True)
-    
+
     tree["column"] = list(rf.columns)
     tree["show"] = "headings"
 
-   # For Headings iterate over the columns
+    # For Headings iterate over the columns
     for col in tree["column"]:
-        tree.heading(col, text=col)
+        if col in tree['displaycolumns']:
+            tree.heading(col, text=col)
 
    # Put Data in Rows
     rf_rows = rf.to_numpy().tolist()
@@ -47,113 +59,110 @@ def open_file():
         rf.dropna(how='all',axis='columns',inplace=True)
     
     tree_view()
-    bill_desc = bill.get()
-    bill_add_cost = add_cost.get()
-    
-    
 
 def add_bill_func():
     ''' 
     add_bill_func() grabs entries from input fields, adds them to db, and adds user typed bill/cost to columns in treeview display.
     Attached to add_btn.
     '''
-    count = len(db)
     b = 0
     ms = 0
-    conn = sqlite3.connect('bill_tracker.db')
-    c = conn.cursor()
+    # conn = sqlite3.connect('bill_tracker.db')
+    # c = conn.cursor()
     global bill_desc
     global bill_add_cost
+    global t
+    
+    count = 0
     bill_desc = bill.get()
-    bill_add_cost = add_cost.get()
+    bill_add_cost = float(add_cost.get())
     db[count] = (bill_desc, bill_add_cost)
 
+    t = t+bill_add_cost
+    total_label.config(text=f'Total: ${format(f"{t:.2f}")}')
+
     if bill_desc == '' or bill_add_cost == '':
-        return
-    if bill_desc.isalnum() == False or bill_add_cost.isalnum() == False:
-        bill.delete(0, END)
-        add_cost.delete(0, END)
         ms = 1
-        return
-    if bill_desc.isdigit() == True:
+    if isinstance(bill_add_cost, float) == False:
         bill.delete(0, END)
         add_cost.delete(0, END)
         ms = 2
-        return
-    for i in bill_add_cost:
-        if i.isalpha() == True:
-            bill.delete(0, END)
-            add_cost.delete(0, END)
-            ms = 3
-            b = 1
+    if isinstance(bill_add_cost, str) == True:
+        bill.delete(0, END)
+        add_cost.delete(0, END)
+        ms = 3
+        b = 1
     # each number in 'ms' represents a different warning, as seen below.
     if ms == 1:
-        messagebox.showwarning('Warning', "Cannot use symbols in field")
-    elif ms == 2:
+        messagebox.showwarning('Warning', "Can't leave field blank")
+    if ms == 2:
         messagebox.showwarning('Warning', "Cannot use numbers in Bill Name field")
-    elif ms == 3:
+        bill.delete(0, END)
+        add_cost.delete(0, END)
+    if ms == 3:
         messagebox.showwarning('Warning', "Cannot use text in Monthly Cost field")
+        bill.delete(0, END)
+        add_cost.delete(0, END)
     else:
         # inserts values directly from entry fields bill and add_cost, not from db
         # if b == 1, warning is triggered above. if b == 0 if warning is not triggered, continue with program
         if b == 0:
-            tree.insert(parent='',index=0, text=f'{bill_desc}',values=(bill_desc, bill_add_cost))
-            lbox.insert(0, [bill_desc, bill_add_cost])
+            tree.insert(parent='',index=0, text=f'{bill_desc}',values=(bill_desc, format(f"{bill_add_cost:.2f}")))
+            
+            # lbox.insert(0, [bill_desc, bill_add_cost])
             # Entry.delete() must be put before database conn.commit() and conn.close() to work
             bill.delete(0, END)
             add_cost.delete(0, END)
             # add bill_name and cost to sqlite db
-            c.execute("INSERT INTO bills VALUES (:bill_desc, :bill_add_cost)",
-                    {
-                        'bill_desc': bill_desc,
-                        'bill_add_cost': bill_add_cost
-                    }
-            )
-            c.execute(""" SELECT oid, * FROM bills""")
-            items = c.fetchall()
-            print(items)
-            conn.commit()
-            conn.close()
+            # c.execute("INSERT INTO bills VALUES (:bill_desc, :bill_add_cost)",
+            #         {
+            #             'bill_desc': bill_desc,
+            #             'bill_add_cost': bill_add_cost
+            #         }
+            # )
+            # c.execute(""" SELECT oid, * FROM bills""")
+            # conn.commit()
+            # conn.close()
             # clear field after pressing add bill button
+    return t
 
 def delete_bill_func():
     '''
         Delete function attached to delete_btn. Deletes selected item from treeview.
     '''
+
     selected = tree.selection()
     tree.delete(selected)
-    conn = sqlite3.connect('bill_tracker.db')
-    c = conn.cursor()
-    c.execute("DELETE FROM bills WHERE bill_name = ?", (selected,))
-    conn.commit()
-    conn.close()
+    
+    
+    # conn = sqlite3.connect('bill_tracker.db')
+    # c = conn.cursor()
+    # c.execute("DELETE FROM bills WHERE bill_name = ?", (selected,))
+    # conn.commit()
+    # conn.close()
 
-def table_exists():
-    '''
-        Checks if table exists, if not it creates table 'bills' in bill_tracker.db
-    '''
-    conn = sqlite3.connect('bill_tracker.db')
-    c = conn.cursor()
+# def table_exists():
+#     '''
+#         Checks if table exists, if not it creates table 'bills' in bill_tracker.db
+#     '''
+#     conn = sqlite3.connect('bill_tracker.db')
+#     c = conn.cursor()
 
-    # create table with data types, sqlite automatically sets primary key
-    # in sqlite3, all tables are enlisted in the ** sqlite_master table **
-    table_exists = c.execute("""SELECT count(name) FROM sqlite_master WHERE type='table' AND name='bills'; """).fetchall()
+#     # create table with data types, sqlite automatically sets primary key
+#     # in sqlite3, all tables are enlisted in the ** sqlite_master table **
+#     table_exists = c.execute("""SELECT count(name) FROM sqlite_master WHERE type='table' AND name='bills'; """).fetchall()
 
-    # check to see if table exists, creates bills table if not
-    if table_exists == []:
-        c.execute("""CREATE TABLE bills (
-        bill_name text,
-        cost integer
-        )""")
-        print("bills TABLE CREATED")
-    else:
-        print("Table exists")
-
-    c.execute("""SELECT * FROM sqlite_master WHERE type='table' AND name='bills'; """)
-    select_all = c.fetchall()
-    print(select_all)
-    conn.commit()
-    conn.close()
+#     # check to see if table exists, creates bills table if not
+#     if table_exists == []:
+#         c.execute("""CREATE TABLE bills (
+#         bill_name text,
+#         cost integer
+#         )""")
+#     else:
+#         pass
+#     c.execute("""SELECT * FROM sqlite_master WHERE type='table' AND name='bills'; """)
+#     conn.commit()
+#     conn.close()
 
 def clear_all():
     for item in tree.get_children():
@@ -169,7 +178,7 @@ def tree_view():
 
     tree.heading('Bill Name', text='Bill Name', anchor=CENTER)
     tree.heading('Cost', text='Cost', anchor=CENTER)
-    tree.place(relx=.3,rely=.50)
+    tree.place(relx=.3,rely=.40)
 
 def save_file():
     '''
@@ -182,6 +191,9 @@ def save_file():
     df.to_excel(writer,sheet_name=f'{datetime.date.today()}')
     writer.save()
 
+def disable_btn():
+    file_menu.entryconfig("Open File", state="disabled")
+
 '''
     Code below generates GUI
 '''
@@ -193,11 +205,14 @@ root.config(menu=m)
 
 file_menu = Menu(m, tearoff=False)
 m.add_cascade(label="File", menu=file_menu)
-file_menu.add_command(label="Open File", command=open_file)
+file_menu.add_command(label="Open File", command=disable_btn)
 file_menu.add_command(label="Save File As", command=save_file)
 
-lbox = Listbox(root, width = 35, selectmode=BROWSE, relief=SUNKEN)
-lbox.place(relx=.52,rely=.05)
+# lbox = Listbox(root, width = 35, selectmode=BROWSE, relief=SUNKEN)
+# lbox.place(relx=.52,rely=.05)
+
+total_label = Label(root, text=f'Total: ${format(f"{t:.2f}")}')
+total_label.place(relx=.68,rely=.88)
 
 bill_label = Label(root, text="Bill Name:")
 bill_label.place(relx=.05, rely=.15)
@@ -225,5 +240,7 @@ clear_btn = Button(root, padx=16, pady=6, text=' Clear ', bd=.5, bg='#e8e8e8', a
 clear_btn.place(relx=.08,rely=.80)
 
 # connect to db, creates bill_tracker.db if it doesn't exist
-table_exists()
+# table_exists()
+
+root.resizable(width=False, height=False)
 root.mainloop()
